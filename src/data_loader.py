459eda28c7c4bf268b8b55daef5879e135fa0d03
yavuzhan <yavuzhankaraoglan@gmail.com>
2026-09-19 13:16:57 +0300

    Add initial data loader and dependencies

---
+++ src/data_loader.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import requests
+
+def test_setup():
+    print("pandas version: ", pd.__version__)
+    print("request calisiyor.")
+    print("veri sistemi hazir.")
+
+
+if __name__ == "__main__":
+    test_setup()
